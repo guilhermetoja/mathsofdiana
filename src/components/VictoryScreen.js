629ea1f7e09fe05ac70704e25d5c99de43371e7b
Guilherme Toja Camilo <guilherme.toja.camilo@gmail.com>
2025-11-17 20:24:24 -0300

    please god let it be over

--- src/components/VictoryScreen.js
+++ src/components/VictoryScreen.js
@@ -21,7 +21,7 @@ const VictoryScreen = ({ onRestart }) => {
             }}
           >
             <img
-              src="/images/Diana.png"
+              src="/images/Diana_okay.png"
               alt="Diana"
               className="w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 object-contain"
             />
